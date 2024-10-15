d7f52b27764e680f77b278988472612bc399a021
Morningstar08 <swastiktripathyfis@gmail.com>
2024-10-15 21:16:17 +0530

    sample

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="flex justify-center items-center">
-      Welcome to Vampire game
-    </div>
-  );
+  return <div className="">Welcome to Vampire game</div>;
 }
